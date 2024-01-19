fd41ca33569e915d73ab0eac93dde421126a5cda
lee dubrovsky <lee.dubrovsky@post.runi.ac.il>
2024-01-19 17:45:22 +0200

    HW 5

--- GameOfLife.java
+++ GameOfLife.java
@@ -8,13 +8,13 @@
 public class GameOfLife {
 
 	public static void main(String[] args) {
-		String fileName = args[0]; ///להוריד שמריצים
+		//String fileName = args[0]; ///להוריד שמריצים
 		//// Uncomment the test that you want to execute, and re-compile.
 		//// (Run one test at a time).
 		// test1(fileName);
 		 //test2(fileName);
 		 //test3(fileName, 3);
-		 play(fileName);
+		// play(fileName);
 	}
 	
 	// Reads the data file and prints the initial board.
